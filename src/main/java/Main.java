474a0af2fe395dcdec61796a16f2afa2ce079ec9
Vivek <vivek@gmail.com>
2026-07-13 18:40:55 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,18 +9,23 @@ public class Main {
     System.out.println("Logs from your program will appear here!");
 
     // TODO: Uncomment the code below to pass the first stage
-    //
-    // try {
-    //   ServerSocket serverSocket = new ServerSocket(4221);
-    //
-    //   // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    //   // ensures that we don't run into 'Address already in use' errors
-    //   serverSocket.setReuseAddress(true);
-    //
-    //   serverSocket.accept(); // Wait for connection from client.
-    //   System.out.println("accepted new connection");
-    // } catch (IOException e) {
-    //   System.out.println("IOException: " + e.getMessage());
-    // }
+
+     try {
+       ServerSocket serverSocket = new ServerSocket(4221);
+
+       // Since the tester restarts your program quite often, setting SO_REUSEADDR
+       // ensures that we don't run into 'Address already in use' errors
+       serverSocket.setReuseAddress(true);
+       Socket socket = serverSocket.accept();
+//       OutputStream outputStream = socket.getOutputStream();
+//       String response = "HTTP/1.1 200 OK\r\n\r\n";
+//       outputStream.write(response.getBytes());
+//       outputStream.flush();
+//       outputStream.close();
+       // Wait for connection from client.
+       System.out.println("accepted new connection");
+     } catch (IOException e) {
+       System.out.println("IOException: " + e.getMessage());
+     }
   }
 }
